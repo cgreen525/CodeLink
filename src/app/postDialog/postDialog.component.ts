aae4f99573a56afe13179897287b64ce637d24eb
Kevin Taing <kevin.taing@ttu.edu>
2017-06-16 22:51:44 -0500

    Scaffolded the dialog box
    Added some input fields and select box. Also renamed the dialog to Listing Dialog, this is only on the frontend though

--- src/app/postDialog/postDialog.component.ts
+++ src/app/postDialog/postDialog.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 
+
 @Component({
   selector: '',
-  templateUrl: '/postDialog.component.html'
+  templateUrl: '/postDialog.component.html',
+  styleUrls: ['./postDialog.component.css']
 })
 export class PostComponent {
   pageTitle: string = 'Hello post dialog!';
+  post: Post = new Post();
+
+  submitPost(){
+    console.log(this.post);
+  }
+}
+
+class Post {
+  title: string;
+  description: string;
+  offer: string;
+
+  constructor() {
+    this.title = "";
+    this.description = "";
+    this.offer = "";
+  }
 }
